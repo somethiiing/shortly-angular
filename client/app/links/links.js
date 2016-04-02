c078145f48574513cb469e9ff61c1d9002242070
somethiiing <wilsonweichengyu@gmail.com>
2016-04-01 20:00:07 -0700

    passed alot of tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,14 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+  $scope.getAll = function() {
+    Links.getAll()
+      .then(function(links) {
+        $scope.data = links;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,7 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+  $scope.link = {};
+  $scope.addLink = function () {};
+  
 });
